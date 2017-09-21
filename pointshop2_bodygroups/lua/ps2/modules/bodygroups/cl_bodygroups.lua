36ec2ef48f0626cec75e14070eb61be5810d1388
Sno <pepsiv6@gmail.com>
2017-09-21 15:43:24 -0400

    Added credits

--- pointshop2_bodygroups/lua/ps2/modules/bodygroups/cl_bodygroups.lua
+++ pointshop2_bodygroups/lua/ps2/modules/bodygroups/cl_bodygroups.lua
@@ -55,3 +55,7 @@ net.Receive("Bodygroups_Init", function(len)
 	
 	hook.Run( "PS2_DoUpdatePreviewModel" )
 end)
+
+hook.Add( "PS2_PopulateCredits", "AddSkinCreds", function( panel ) 
+    panel:AddCreditSection( "Bodygroups Module", [[ Bodygroups Module by Snowywolf (https://github.com/snowywolf/Pointshop-2-Bodygroups). ]] ) 
+end )
