61750daf3c53c69acefb63c6ceeb6dba21c62af1
Sno <pepsiv6@gmail.com>
2017-09-21 15:37:08 -0400

    Rewrote serverside
    Fixed a few issues and done some optimizing.

--- pointshop2_bodygroups/lua/ps2/modules/bodygroups/sv_bodygroups.lua
+++ pointshop2_bodygroups/lua/ps2/modules/bodygroups/sv_bodygroups.lua
@@ -3,91 +3,65 @@ util.AddNetworkString("Bodygroups_Set")
 
 local BodygroupsModel = Pointshop2.BodygroupsModel
 
-local meta = FindMetaTable("Player")
-local entity = FindMetaTable("Entity")
+function LoadBodygroups( user )
+	if !user:IsValid() then return end
 
-function meta:LoadBodygroups()
-	if !self:IsValid() then return end
-	
-	self.BodygroupsData = {}
-	
-	BodygroupsModel.findByOwnerId( self.kPlayerId )
+	BodygroupsModel.findByOwnerId( user.kPlayerId )
 	:Then( function( Data )
-		if Data == nil then return end
-		
-		self.BodygroupsData[Data.modelId] = {Data.skin, Data.groups}
-		
-		net.Start( "Bodygroups_Init" )
-		    for k, v in pairs( self.BodygroupsData ) do
-			    net.WriteUInt( k, 16 )
-				net.WriteUInt( v[1], 8 )
-				net.WriteString( v[2] )
-			end
-		net.Send( self )
 
-	end )
-	
-end
+		if Data == nil then 
 
-function meta:SaveBodyGroups( modelID )
-	if !self:IsValid() then return end
+		    user.BodygroupsData = BodygroupsModel:new( )
+		    user.BodygroupsData.ownerId = user.kPlayerId 
 
-	return BodygroupsModel.findWhere{ ownerId = self.kPlayerId }
-	:Then( function( bodygroups )
-		local bodygroup = bodygroups[1]
-		if not bodygroup then
-			--Create it
-			bodygroup = BodygroupsModel:new( )
-			bodygroup.ownerId = self.kPlayerId 
-			bodygroup.modelId = modelID
+		    return
 		end
-		bodygroup.skin   =  self.BodygroupsData[modelID][1]
-		bodygroup.groups =  self.BodygroupsData[modelID][2]
+
+		user.BodygroupsData = Data
+
+		net.Start( "Bodygroups_Init" )
+			    net.WriteUInt( Data.modelId, 16 )
+				net.WriteUInt( Data.skin, 8 )
+				net.WriteString( Data.groups )
+		net.Send( user )
+
+	end )
+
+end
+hook.Add( "PS2_PlayerFullyLoaded", "BG_LoadBodygroups", LoadBodygroups )
+
+function SetBodyGroups( user )
+    timer.Simple(0, function()
+	
+        if !user:IsValid() or user:PS2_GetItemInSlot( "Model" ) == nil then return true end
+
+	    if user.BodygroupsData and user.BodygroupsData.modelId == user:PS2_GetItemInSlot( "Model" ).id then
 		
-		return bodygroup:save( )
+		    user:SetBodyGroups(user.BodygroupsData.groups)
+		    user:SetSkin(user.BodygroupsData.skin)
+			
+		    return true
+	    end
+		
 	end )
 end
+hook.Add( "PS2_PlayermodelShouldShow", "BG_SetBodyGroups", SetBodyGroups )
 
-function meta:UpdateBodygroups(ID, Skin, Groups)
-	if !self:IsValid() then return end
-	
-	if not self.BodygroupsData then
-		self:LoadBodygroups()
-		
-		timer.Simple(5, function()
-			if not self:IsValid() then return end
-			self:UpdateBodygroups(ID, Skin, Groups)
-		end)
-		return
-	end
+net.Receive("Bodygroups_Set", function(len, client)
+	if client.AntiSpam != nil and client.AntiSpam > SysTime() then return end
 
-	self.BodygroupsData[ID] = {Skin, Groups}
-	
-	self:SaveBodyGroups(ID)
-end
+	client.AntiSpam = SysTime() + 2
 
-function meta:SetSkin(skin)
-    if self:PS2_GetItemInSlot( "Model" ) == nil then return end
-	local ID = self:PS2_GetItemInSlot( "Model" ).id
-	if self.BodygroupsData and self.BodygroupsData[ID] != nil then
-		self:SetBodyGroups(self.BodygroupsData[ID][2])
-		entity.SetSkin(self, self.BodygroupsData[ID][1])
-	end
-end
+    local ItemID = net.ReadInt(32)
+    local Groups = net.ReadString()
+	local Skin   = net.ReadString()
+	
+	client:SetBodyGroups(Groups)
+	client:SetSkin(Skin)
+	
+	client.BodygroupsData.modelId = ItemID
+    client.BodygroupsData.groups  = Groups
+	client.BodygroupsData.skin    = Skin
 
-net.Receive("Bodygroups_Set", function(len, ply)
-    local ModelID = net.ReadInt(32)
-    local groups = net.ReadString()
-	local skin = net.ReadString()
-	if ply.BGSpam != nil and ply.BGSpam > SysTime() then return end
-	
-	ply:UpdateBodygroups(ModelID, skin, groups)
-	ply:SetSkin() --Overwritten skin function to set groups and skin.
-	
-	ply.BGSpam = SysTime() + 4
+	client.BodygroupsData:save()
 end)
-
-hook.Add( "PS2_PlayerFullyLoaded", "Playerloaded22", function( ply )
-    if !ply:IsValid() then return end
-	ply:LoadBodygroups()
-end )
